0767be364cd8c02a0539404a3140cdc8250f6711
Tomas Ayala <lqmmgep@gmail.com>
2024-04-20 15:54:18 -0400

    Changed ConfigFile operator[]; Change list tokens from parenthesis to curly braces

--- libs/include/posta/Util/ConfigFile.h
+++ libs/include/posta/Util/ConfigFile.h
@@ -8,9 +8,19 @@
 #include <posta/Util/General.h>
 
 namespace posta {
+	/// ConfigFile is a very simple class capable of loading named attributes from a file
+	/// The format of the configuration file is the following:
+	/// - Each line represents a named value (or named list of values)
+	/// - Each line starts with a name (alphanumeric name) and equals sign (=) and at the right side one of two possibilities:
+	///   - A value, which can be anything, each value is interpreted as the program suggest and there is no need to use the utilities of the class.
+	///   - A list, a list is an enclosed space with ',' as separators and curly braces as the enclosing parenthesis, anything other than '{', '}', and ',' is interpreted as part of one of the strings contained within the list.
+	/// example.txt:
+	/// a = b
+	/// hello = 54.32
+	/// ls = {a, b, c}
+	/// Let config_file = ConfigFile("example.txt"), then to access 'hello' as a string use config_file["hello"].to_string(), to access the first element of 'ls' do config_file["ls"][0]
 	class ConfigFile
 	{
-	public:
 		struct Data
 		{
 			Data(std::string value);
@@ -19,19 +29,33 @@ namespace posta {
 			std::string to_string();
 			int to_int();
 			float to_float();
+			double to_double();
 			// If data is a list
 			Data& operator[](size_t index);
 			std::vector<Data>::iterator begin();
 			std::vector<Data>::iterator end();
 			size_t size();
+			glm::vec2 to_vec2();
+			glm::vec3 to_vec3();
+			glm::vec4 to_vec4();
+			glm::ivec2 to_ivec2();
+			glm::ivec3 to_ivec3();
+			glm::ivec4 to_ivec4();
+			glm::dvec2 to_dvec2();
+			glm::dvec3 to_dvec3();
+			glm::dvec4 to_dvec4();
 
+			bool is_list;
 			std::string value;
 			std::vector<Data> values;
 		};
 		static Data to_data(std::string str);
+	public:
+		static Data to_value(std::string str);
+		static Data to_list(std::string str);
 		ConfigFile(std::filesystem::path path);
 
-		std::string& operator[](std::string key);
+		Data operator[](std::string key);
 		std::string to_string();
 
 		std::unordered_map<std::string, std::string> data;
--- libs/src/posta/Util/ConfigFile.cpp
+++ libs/src/posta/Util/ConfigFile.cpp
@@ -5,11 +5,13 @@ using posta::ConfigFile;
 ConfigFile::Data::Data(std::string value)
 {
 	this->value = value;
+	is_list = false;
 }
 
 ConfigFile::Data::Data(std::vector<ConfigFile::Data> values)
 {
 	this->values = values;
+	is_list = true;
 }
 
 std::string ConfigFile::Data::to_string()
@@ -19,39 +21,141 @@ std::string ConfigFile::Data::to_string()
 
 int ConfigFile::Data::to_int()
 {
+	if (is_list)
+		throw std::runtime_error(("tried to access int from list Data object (from ConfigFile); '" + to_string() + "'").c_str());
 	return stoi(value);
 }
 
 float ConfigFile::Data::to_float()
 {
+	if (is_list)
+		throw std::runtime_error(("tried to access float from list Data object (from ConfigFile); '" + to_string() + "'").c_str());
 	return stof(value);
+}
+
+double ConfigFile::Data::to_double()
+{
+	if (is_list)
+		throw std::runtime_error(("tried to access double from list Data object (from ConfigFile); '" + to_string() + "'").c_str());
+	return stod(value);
 }
 
 ConfigFile::Data& ConfigFile::Data::operator[](size_t index)
 {
+	if (!is_list)
+		throw std::runtime_error(("tried to access element from non-list Data object (from ConfigFile); '" + to_string() + "'").c_str());
 	return values.at(index);
 }
 
 std::vector<ConfigFile::Data>::iterator ConfigFile::Data::begin()
 {
+	if (!is_list)
+		throw std::runtime_error(("tried to access begin() from non-list Data object (from ConfigFile); '" + to_string() + "'").c_str());
 	return values.begin();
 }
 
 std::vector<ConfigFile::Data>::iterator ConfigFile::Data::end()
 {
+	if (!is_list)
+		throw std::runtime_error(("tried to access end() from non-list Data object (from ConfigFile); '" + to_string() + "'").c_str());
 	return values.end();
 }
 
 size_t ConfigFile::Data::size()
 {
+	if (!is_list)
+		throw std::runtime_error(("tried to query size from non-list Data object (from ConfigFile); '" + to_string() + "'").c_str());
 	return values.size();
+}
+
+glm::vec2 ConfigFile::Data::to_vec2()
+{
+	if (!is_list)
+		throw std::runtime_error(("tried to access glm::vec2 from non-list Data object (from ConfigFile); '" + to_string() + "'").c_str());
+	if (values.size() != 2)
+		throw std::runtime_error(("tried to access glm::vec2 from list Data object[with size()=" + std::to_string(size()) + " instead of 2] (from ConfigFile); '" + to_string() + "'").c_str());
+	return glm::vec2(values[0].to_float(), values[1].to_float());
+}
+glm::vec3 ConfigFile::Data::to_vec3()
+{
+	if (!is_list)
+		throw std::runtime_error(("tried to access glm::vec3 from non-list Data object (from ConfigFile); '" + to_string() + "'").c_str());
+	if (values.size() != 3)
+		throw std::runtime_error(("tried to access glm::vec3 from list Data object[with size()=" + std::to_string(size()) + " instead of 3] (from ConfigFile); '" + to_string() + "'").c_str());
+	return glm::vec3(values[0].to_float(), values[1].to_float(), values[2].to_float());
+}
+glm::vec4 ConfigFile::Data::to_vec4()
+{
+	if (!is_list)
+		throw std::runtime_error(("tried to access glm::vec4 from non-list Data object (from ConfigFile); '" + to_string() + "'").c_str());
+	if (values.size() != 4)
+		throw std::runtime_error(("tried to access glm::vec4 from list Data object[with size()=" + std::to_string(size()) + " instead of 4] (from ConfigFile); '" + to_string() + "'").c_str());
+	return glm::vec4(values[0].to_float(), values[1].to_float(), values[2].to_float(), values[3].to_float());
+}
+glm::ivec2 ConfigFile::Data::to_ivec2()
+{
+	if (!is_list)
+		throw std::runtime_error(("tried to access glm::ivec2 from non-list Data object (from ConfigFile); '" + to_string() + "'").c_str());
+	if (values.size() != 2)
+		throw std::runtime_error(("tried to access glm::ivec2 from list Data object[with size()=" + std::to_string(size()) + " instead of 2] (from ConfigFile); '" + to_string() + "'").c_str());
+	return glm::ivec2(values[0].to_int(), values[1].to_int());
+}
+glm::ivec3 ConfigFile::Data::to_ivec3()
+{
+	if (!is_list)
+		throw std::runtime_error(("tried to access glm::ivec3 from non-list Data object (from ConfigFile); '" + to_string() + "'").c_str());
+	if (values.size() != 3)
+		throw std::runtime_error(("tried to access glm::ivec3 from list Data object[with size()=" + std::to_string(size()) + " instead of 3] (from ConfigFile); '" + to_string() + "'").c_str());
+	return glm::ivec3(values[0].to_int(), values[1].to_int(), values[2].to_int());
+}
+glm::ivec4 ConfigFile::Data::to_ivec4()
+{
+	if (!is_list)
+		throw std::runtime_error(("tried to access glm::ivec4 from non-list Data object (from ConfigFile); '" + to_string() + "'").c_str());
+	if (values.size() != 4)
+		throw std::runtime_error(("tried to access glm::ivec4 from list Data object[with size()=" + std::to_string(size()) + " instead of 4] (from ConfigFile); '" + to_string() + "'").c_str());
+	return glm::ivec4(values[0].to_int(), values[1].to_int(), values[2].to_int(), values[3].to_int());
+}
+glm::dvec2 ConfigFile::Data::to_dvec2()
+{
+	if (!is_list)
+		throw std::runtime_error(("tried to access glm::dvec2 from non-list Data object (from ConfigFile); '" + to_string() + "'").c_str());
+	if (values.size() != 2)
+		throw std::runtime_error(("tried to access glm::dvec2 from list Data object[with size()=" + std::to_string(size()) + " instead of 2] (from ConfigFile); '" + to_string() + "'").c_str());
+	return glm::dvec2(values[0].to_double(), values[1].to_double());
+}
+glm::dvec3 ConfigFile::Data::to_dvec3()
+{
+	if (!is_list)
+		throw std::runtime_error(("tried to access glm::dvec3 from non-list Data object (from ConfigFile); '" + to_string() + "'").c_str());
+	if (values.size() != 3)
+		throw std::runtime_error(("tried to access glm::dvec3 from list Data object[with size()=" + std::to_string(size()) + " instead of 3] (from ConfigFile); '" + to_string() + "'").c_str());
+	return glm::dvec3(values[0].to_double(), values[1].to_double(), values[2].to_double());
+}
+glm::dvec4 ConfigFile::Data::to_dvec4()
+{
+	if (!is_list)
+		throw std::runtime_error(("tried to access glm::dvec4 from non-list Data object (from ConfigFile); '" + to_string() + "'").c_str());
+	if (values.size() != 4)
+		throw std::runtime_error(("tried to access glm::dvec4 from list Data object[with size()=" + std::to_string(size()) + " instead of 4] (from ConfigFile); '" + to_string() + "'").c_str());
+	return glm::dvec4(values[0].to_double(), values[1].to_double(), values[2].to_double(), values[3].to_double());
 }
 
 ConfigFile::Data ConfigFile::to_data(std::string str)
 {
+	if (str.at(0) == '{' && str.at(str.size() - 1) == '}')
+		return to_list(str);
+	return to_value(str);
+}
+
+ConfigFile::Data ConfigFile::to_value(std::string str)
+{
+	return Data(str);
+}
+
+ConfigFile::Data ConfigFile::to_list(std::string str)
+{
 	str = posta::strip(str);
-	if (str.empty() || str[0] != '(' || str[str.size() - 1] != ')')
-		return Data(str);
 	str.insert(str.end() - 1, ',');
 
 	int parenthesis = 0;
@@ -59,9 +163,9 @@ ConfigFile::Data ConfigFile::to_data(std::string str)
 	std::vector<std::string> elements;
 	for (auto& c : str)
 	{
-		if (c == '(')
+		if (c == '{')
 			parenthesis++;
-		else if (c == ')')
+		else if (c == '}')
 			parenthesis--;
 		else if (c == ',' && parenthesis == 1)
 		{
@@ -111,7 +215,7 @@ ConfigFile::ConfigFile(std::filesystem::path path)
 		// Dividing the string in two parts
 		size_t pos = line.find('=');
 		if (pos == std::string::npos)
-			throw std::logic_error(std::string("Couldn't find the '=' in one of the config pairs, in line ") + std::to_string(line_pos + 1));
+			throw std::logic_error(std::string("couldn't find the '=' in one of the config pairs, in line ") + std::to_string(line_pos + 1));
 		std::string left, right;
 		left.insert(left.begin(), line.begin(), line.begin() + pos);
 		right.insert(right.begin(), line.begin() + pos + 1, line.end());
@@ -123,11 +227,11 @@ ConfigFile::ConfigFile(std::filesystem::path path)
 	file.close();
 }
 
-std::string& ConfigFile::operator[](std::string key)
+ConfigFile::Data ConfigFile::operator[](std::string key)
 {
 	if (data.count(key) == 0)
 		throw std::out_of_range(std::string("couldn't get key '") + key + "'");
-	return data.at(key);
+	return ConfigFile::to_data(posta::strip(data.at(key)));
 }
 
 std::string ConfigFile::to_string()
